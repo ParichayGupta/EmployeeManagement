7c3abcf52422a41f829b22d1e0b65dbe453025b2
Parichay Gupta <44670041+ParichayGupta@users.noreply.github.com>
2022-05-05 23:03:55 +0530

    added swagger via openAPI

--- src/main/java/com/employee/EmployeeManagementApplication.java
+++ src/main/java/com/employee/EmployeeManagementApplication.java
@@ -1,10 +1,7 @@
 package com.employee;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
