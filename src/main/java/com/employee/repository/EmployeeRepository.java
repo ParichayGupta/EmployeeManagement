92d9c56f7f3ada8555fe325b05b01121fbff1db6
Parichay Gupta <44670041+ParichayGupta@users.noreply.github.com>
2022-05-08 23:32:02 +0530

    update

--- src/main/java/com/employee/repository/EmployeeRepository.java
+++ src/main/java/com/employee/repository/EmployeeRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-	Optional<Employee> findByName(String name);
+	public Optional<Employee> findByName(String name);
 }
