8c4d87ad482582ee288970908524556ffaf66cae
Parichay Gupta <44670041+ParichayGupta@users.noreply.github.com>
2022-05-14 22:49:30 +0530

    added data

---
+++ target/classes/static/data.sql
@@ -0,0 +1,9 @@
+-- insert into employee (id,name,age) values 
+-- (1, 'Ajay', 23),(2, 'Ravi', 45),(3, 'Raman',43),(4, 'Raja', 29),
+-- (5, 'Sumit', 25),(6, 'Nidhi', 64),(7, 'Mohit',19),(8, 'Hitesh', 32),
+-- (9, 'Nikhil', 32),(10, 'Tanmay', 32),(11, 'Rohit', 34),(12, 'Shubh', 20),
+-- (13, 'Aastha', 24),(14, 'Saloni', 36),(15, 'Rini', 29);
+
+-- insert into location (locationId, location) values 
+-- (1,'Pune'),(2,'Chennai'),(3,'Indore'),(4,'Banglore'),(5,'Noida'),
+-- (6,'Kolkata'),(7,'Bhopal'),(8,'Jaipur'),(9,'Delhi'),(10,'Mumbai');
